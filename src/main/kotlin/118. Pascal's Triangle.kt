9e97cd1732685246e230800fd8e7cd99a97c56d6
saisooriyas <saisooriya2003@gmail.com>
2023-09-09 12:51:59 +0530

    08-09-23

---
+++ src/main/kotlin/118. Pascal's Triangle.kt
@@ -0,0 +1,41 @@
+import java.util.Scanner
+
+class Solution {
+    fun generate(numRows: Int): List<List<Int>> {
+        val pascalval : MutableList<Int> = mutableListOf()
+        val pascal : MutableList<MutableList<Int>> = mutableListOf()
+        var value : Int
+
+        for (times in 0..<numRows) {
+            val tempPascalval = pascalval.toMutableList()
+            for (index in 0..times) {
+                tempPascalval.add(check(times, index))
+            }
+            pascal.add(tempPascalval)
+        }
+        return pascal;
+    }
+
+    fun check(n : Int, k : Int): Int {
+        var res = 1
+        var c = k
+        if (k> n-k)
+            c = n-k
+
+        for (i in 0..<c){
+            res *= (n - i)
+            res /= (i + 1)
+        }
+        return res
+    }
+
+}
+
+
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val sol = Solution()
+    val value = sc.nextInt()
+    print(sol.generate(value))
+}
